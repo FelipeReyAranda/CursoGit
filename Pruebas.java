1842f0078b205acb66692e5f1b5a0bf9a93da28e
FelipeReyAranda <felipe.rey.aranda@alumnos.upm.es>
2022-02-17 13:23:12 +0100

    Subiendo el archivo java

---
+++ Pruebas.java
@@ -0,0 +1,7 @@
+class HelloWorld
+{
+    public static void main(String[] args) {
+    System.out.println("Manda narices que tenga que estar haciendo un hola mundo después de todo el tiempo que llevo estudiando java");
+    System.out.println("Hello world");
+    }
+}
